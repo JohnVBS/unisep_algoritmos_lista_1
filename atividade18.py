50723db2959f827f7a87f2726a3435bdd3985e7b
JohnVBS <joaovitor.save@hotmail.com>
2024-03-19 12:56:28 -0300

    foi

--- atividade18.py
+++ atividade18.py
@@ -1,1 +1,13 @@
-ola
+G1 = float(input("Insira o valor da primeira avaliação: "))
+G2 = float(input("Insira o valor da segunda avaliação: "))
+
+media = (G1 + (G2 * 2)) / 3
+
+if media >= 7:
+    {
+        print(f"Parabéns, você passou! Sua média é: ", {media})
+    }
+else:
+    {
+        print(f"Que pena, você não passou! Sua média é: ", {media})
+    }
---
+++ atividade22.py
@@ -0,0 +1,3 @@
+num = float(input("Insira um número que representa o tamanho do lado de um quadrado: "))
+area = num * num
+print(f"A área deste quadrado é de: ", area, "metros quadrados")
